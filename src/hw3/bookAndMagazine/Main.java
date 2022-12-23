691b1fcbc36b9f20e9bdb30126734d77fb063dd6
DELL <users1811@gmail.com>
2022-12-23 06:48:38 +0200

    hw3

--- src/hw3/bookAndMagazine/Main.java
+++ src/hw3/bookAndMagazine/Main.java
@@ -6,15 +6,12 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Printable> arrayOfPrintable = new ArrayList<>();
         ArrayList<String> authorsBook1 = new ArrayList<>();
-        authorsBook1.add("Vovk Andrii");
+        authorsBook1.add("Stasik Pes");
+        authorsBook1.add("Vasya Pacan");
         Book book1 = new Book("Vasya", authorsBook1,500,2000);
         Magazine shop1 = new Magazine(22,"Abetka");
         arrayOfPrintable.add(book1);
         arrayOfPrintable.add(shop1);
-        arrayOfPrintable.get(0).print();
-        arrayOfPrintable.get(1).print();
-        System.out.println(arrayOfPrintable.get(0));
-        Printable.testPrint();
-
+        arrayOfPrintable.forEach(Printable::print);
     }
 }
